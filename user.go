e6f11efbeb47cdab2093b191b080c6026f98968a
stephenhu <epynonymous@outlook.com>
2022-09-13 07:25:41 +0800

    flatten user structure

--- user.go
+++ user.go
@@ -1,7 +1,13 @@
 package ouwsdk
 
 
-type CharAttributes struct {
+type User struct {
+	//ID 								string			`redis:"id"`
+	//Email         		string      `redis:"email"`
+	Name          			string      `redis:"name"`
+	Pass 								string			`redis:"password"`
+	//Job          			string      `redis:"job"`
+	Municipal     			string      `redis:"municipal"`
 	Female        			bool      	`redis:"female"`
 	Age           			int         `redis:"age"`
 	Height        			int         `redis:"height"`
@@ -26,56 +32,13 @@ type CharAttributes struct {
 	Temperance          int         `redis:"temperance"`
 	Recovery            int         `redis:"recovery"`
 	Innovation          int         `redis:"innovation"`
-}
-
-
-type MilitaryAttribute struct {
-	Size								int 				`redis:"size"`
-	Rating          		int         `redis:"rating"`
-}
-
-
-type MilitaryAttributes struct {
-	Dagger				int		`redis:"dagger"`
-	Sword					int		`redis:"sword"`
-	Scimitar			int		`redis:"scimitar"`
-	Spear					int		`redis:"spear"`
-	HookedSpear		int		`redis:"hookedspear"`
-	Staff					int		`redis:"staff"`
-	DragonBlade		int		`redis:"dragonblade"`
-	Club					int		`redis:"club"`
-	Hammer				int		`redis:"hammer"`
-	Axe						int		`redis:"axe"`
-	Halberd				int		`redis:"halberd"`
-	Bow						int		`redis:"bow"`
-	CrossBow			int		`redis:"crossbow"`
-	HorseBow			int		`redis:"horsebow"`
-	Horse					int		`redis:"horse"`
-	Chariot				int		`redis:"chariot"`
-	Shield				int		`redis:"shield"`
-}
-
-
-type MartialArtsAttributes struct {
-	Bear					int		`redis:"bear"`
-	Crane					int		`redis:"crane"`
-	Drunken				int		`redis:"drunken"`
-	IronFist			int		`redis:"ironfist"`
-	Leopard				int		`redis:"leopard"`
-	Mantis				int		`redis:"mantis"`
-	Monkey				int		`redis:"monkey"`
-	Snake					int		`redis:"snake"`
-	Tiger					int		`redis:"tiger"`
-}
-
-
-type User struct {
-	//ID 						string			`redis:"id"`
-	//Email         string      `redis:"email"`
-	Pass 					string			`redis:"password"`
-	Name          string      `redis:"name"`
-	//Job          	string      `redis:"job"`
-	Municipal     string      `redis:"municipal"`
-	Attributes    CharAttributes
-	Combat      	MilitaryAttributes
+	Bear								int					`redis:"bear"`
+	Crane								int					`redis:"crane"`
+	Drunken							int					`redis:"drunken"`
+	IronFist						int					`redis:"ironfist"`
+	Leopard							int					`redis:"leopard"`
+	Mantis							int					`redis:"mantis"`
+	Monkey							int					`redis:"monkey"`
+	Snake								int					`redis:"snake"`
+	Tiger								int					`redis:"tiger"`
 }
